7ddda16e23bdf50457ad1d25e323e68378789c7a
Akash Jain <ajainphysics@gmail.com>
2019-01-30 15:49:26 -0800

    Basic cleanup.
    Removed some useless files.
    Cleaned up resources.

--- web/foot.php
+++ web/foot.php
@@ -18,30 +18,14 @@
 <!-- jQuery.dotdotdot: Ellipsis for text TODO: This is too resource consuming -->
 <script src="js/jQuery.dotdotdot/jquery.dotdotdot.min.js" type="text/javascript"></script>
 
+<script src="js/awesomplete/awesomplete.min.js" async></script>
+
 <script src="js/global.js"></script>
 
 <script src="api/lib/mySpires.js"></script>
 <script src="api/lib/mySpires_Bar.js"></script>
 <script src="js/components.js"></script>
 <script src="refSpires/refSpires.js"></script>
-
-
-<!--
-<script>
-
-    let entryTagsAuthors = {};
-    entryTagsAuthors = <?php echo json_encode(mySpires::taglist()); ?>;
-    let entryTags = Object.keys(entryTagsAuthors);
-
-    for(let key of entryTags) {
-        entryTagsAuthors[key] = entryTagsAuthors[key].map(function(t) {
-            return utf8_decode(t);
-        });
-    }
-
-</script>
-
--->
 
 <?php if(file_exists(__DIR__."/js/".pageLabel.".js")) { ?>
     <script src="js/<?php echo pageLabel; ?>.js"></script>
@@ -69,28 +53,6 @@
         src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-MML-AM_CHTML">
 </script>
 
-<script src="js/awesomplete/awesomplete.min.js" async></script>
-
 <?php webapp::fetch_scripts(); ?>
 
 <script src='https://www.google.com/recaptcha/api.js'></script>
-
-<!--
-<script>
-    function loadMathJax() {
-        var script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-MML-AM_CHTML";   // use the location of your MathJax
-
-        var config = "MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\\\(','\\\\)']]}});" +
-            "MathJax.Hub.Startup.onload();";
-
-        if (window.opera) {script.innerHTML = config}
-        else {script.text = config}
-
-        document.getElementsByTagName("head")[0].appendChild(script);
-    }
-    loadMathJax();
-</script>
-
--->
